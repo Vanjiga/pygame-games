0f0dae7f6f65906cf1293864083bdff3d55710f8
Vanjiga <vanja.gacic123@gmal.com>
2021-01-30 14:11:55 +0100

    fix

--- snake.py
+++ snake.py
@@ -5,14 +5,14 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 clock = pygame.time.Clock()
 
-surface = pygame.surface((200,150))
+surface = pygame.surface
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.quit:
             pygame.quit()
             sys.exit()
-    screen.fill(pygame.Color('green'))
-    screen.blit(surface,(200,250 ))
+    screen.fill(pygame.Color,(("yellow"))
+    screenblit(surface,(200,250 ))
     pygame.display.update()
     clock.tick(60)
